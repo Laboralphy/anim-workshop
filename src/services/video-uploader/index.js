42cec0bc2994d5462a00b90342c15cdd2137168e
MARANDET Raphael <raphael.marandet@gendarmerie.interieur.gouv.fr>
2018-10-11 17:43:19 +0200

    comments

--- src/services/video-uploader/index.js
+++ src/services/video-uploader/index.js
@@ -8,26 +8,41 @@ const config = require('../config');
 class VideoUploader {
     async upload(pCallback) {
         return new Promise(async (resolve, reject) => {
-            let localFile = projectTree.getOutputFilename();
-            let sDisplayableName = localFile.split('/').pop();
+            let localFile = projectTree.getOutputFilename();  // complete filename with path
+            let sDisplayableName = localFile.split('/').pop(); // filename without path
+
+            // testing local file existance
             let bExists = await fsp.readable(localFile);
             if (!bExists) {
-                reject('fichier video inexistant : ' + sDisplayableName);
+                // video is not available (not created yet)
+                reject('fichier vidéo inexistant : ' + sDisplayableName);
                 return;
             }
+            // creating remote filename
             let oCnx = config.upload;
             let remoteFile = oCnx.remotePath + '/' + projectTree.getName() + '.mp4';
+
+            // opening sftp connection
             let sftp = new SFTPClient();
             await sftp.connect(oCnx);
+
+            // calc file size
             let nSize = await fsp.size(localFile);
             let nSent = 0;
+
+            // opening local file
             let rs = fs.createReadStream(localFile);
             if (pCallback) {
+                // notifying start
                 pCallback('start', {filename: sDisplayableName, size: nSize});
+
+                // when data is transfered, add chunk size to accumulator
                 rs.on('data', chunk => {
                     nSent += chunk.length;
                     pCallback('progress', {sent: nSent, size: nSize});
                 });
+
+                // when no more data, notify final
                 rs.on('end', () => {
                     nSent = nSize;
                     pCallback('end');
